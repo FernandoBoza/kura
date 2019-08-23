9284916916b9927556c1baf45b84afc8f3a61903
Fernando Boza <hi@fernandoboza.com>
2019-08-22 23:47:44 -0400

    finished CRUD of Hospital

--- src/main/java/com/fernandoboza/kura/hospapi/Hospital/HospitalController.java
+++ src/main/java/com/fernandoboza/kura/hospapi/Hospital/HospitalController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.fernandoboza.kura.hospapi.Utils.Utils.createLatCord;
 
@@ -22,20 +23,43 @@ public class HospitalController {
     @PostMapping(path = "", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Iterable<Hospital> createHospital(@RequestBody List<Hospital> hospitals) throws InterruptedException, ApiException, IOException {
         List<Hospital> newHosp = new ArrayList<>();
-        for (Hospital hosp: hospitals){
+        for (Hospital hosp : hospitals) {
             newHosp.add(createLatCord(hosp));
         }
         return hospitalRepository.saveAll(newHosp);
     }
 
-    @GetMapping(path="")
+    @GetMapping(path = "")
     public Iterable<Hospital> findAllHospital() {
         return hospitalRepository.findAll();
     }
 
     @GetMapping(path = "{id}")
-    public Optional<Hospital> findById(@PathVariable String id){
+    public Optional<Hospital> findById(@PathVariable String id) {
         int parsedInt = Integer.parseInt(id);
         return hospitalRepository.findById(parsedInt);
+    }
+    @PutMapping(path = "{id}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Hospital updateHospital(@PathVariable String id, @RequestBody Hospital hospital) {
+        if (hospitalRepository.existsById(Integer.parseInt(id))) {
+            return hospitalRepository.save(hospital);
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping(path = "{id}")
+    public String deleteHospital(@PathVariable String id){
+        Optional<Hospital> hospName = hospitalRepository.findById(Integer.parseInt(id));
+        var ref = new Object() {
+            String name;
+        };
+        hospName.flatMap(hospital -> {
+            ref.name = hospital.getName();
+            return Optional.empty();
+        });
+        hospitalRepository.deleteById(Integer.parseInt(id));
+
+        return "Deleted hospital " + ref.name + " | id : " + id;
     }
 }
